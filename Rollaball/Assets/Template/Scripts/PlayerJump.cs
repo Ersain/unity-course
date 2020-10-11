8877a679f88cbed6f529139b5f4ad63af0315389
ersain <ersain.subs@gmail.com>
2020-10-11 14:31:59 +0600

    Lab 2

---
+++ Rollaball/Assets/Template/Scripts/PlayerJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+
+    private float jumpSpeed = 5;
+    private Rigidbody rb;
+    private bool onGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && onGround) {
+	    rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+	    onGround = false;
+	}
+    }
+
+    void OnCollisionEnter(Collision collision) {
+	onGround = true;
+    }
+}
